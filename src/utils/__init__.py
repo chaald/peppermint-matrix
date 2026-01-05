99a7db3572d2e97fdedc67b2ff3583f33f2a57f8
Hafidh Rendyanto <hafidh.rendyanto@finaccel.co>
2026-01-05 10:02:03 +0000

    speed up matrix factorization

---
+++ src/utils/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+def is_notebook() -> bool:
+    "Check if the code is running in a Jupyter notebook environment."
+    
+    try:
+        from IPython import get_ipython
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False      # Probably standard Python interpreter
