7a7267ca2c3439f6b7fcc8bd1de87e4e6e5dec49
Hafidh Rendyanto <hafidh.rendyanto@finaccel.co>
2026-01-10 07:07:50 +0000

    couple sampler

--- src/models/matrix_factorization.py
+++ src/models/matrix_factorization.py
@@ -1,26 +1,135 @@
 import numpy as np
 import tensorflow as tf
 import keras
+from tqdm import tqdm
 
-class MatrixFactorizationModel(keras.Model):
+from src.sampler import SimpleSampler
+
+class MatrixFactorization(keras.Model):
     def __init__(self, user_count: int, item_count: int, embedding_dimension_count: int, l1_regularization: float = 0.00, l2_regularization: float = 0.00):
-        super(MatrixFactorizationModel, self).__init__()
-        self.user_embedding = keras.layers.Embedding(
+        super(MatrixFactorization, self).__init__()
+        self.user_embedding_layer = keras.layers.Embedding(
             input_dim=user_count,
             output_dim=embedding_dimension_count,
             embeddings_initializer='uniform',
-            embeddings_regularizer=keras.regularizers.l2(l2_regularization) + keras.regularizers.l1(l1_regularization)
+            embeddings_regularizer=keras.regularizers.l1_l2(l1=l1_regularization, l2=l2_regularization)
         )
-        self.item_embedding = keras.layers.Embedding(
+        self.item_embedding_layer = keras.layers.Embedding(
             input_dim=item_count,
             output_dim=embedding_dimension_count,
             embeddings_initializer='uniform',
-            embeddings_regularizer=keras.regularizers.l2(l2_regularization) + keras.regularizers.l1(l1_regularization)
+            embeddings_regularizer=keras.regularizers.l1_l2(l1=l1_regularization, l2=l2_regularization)
         )
 
+        # Loss & Metrics Tracker
+        self.train_loss_tracker = keras.metrics.Mean(name="train_loss")
+        self.test_loss_tracker = keras.metrics.Mean(name="test_loss")
+
+        # Loss & Metrics History
+        self.train_loss_history = []
+        self.test_loss_history = []
+
+
+    def compile(
+        self, 
+        optimizer: keras.optimizers.Optimizer,
+        sampler: SimpleSampler,
+        **kwargs
+    ):
+        self.optimizer = optimizer
+        self.sampler = sampler
+
     def call(self, user_ids: tf.Tensor, item_ids: tf.Tensor) -> tf.Tensor:
-        user_embedding = self.user_embedding(user_ids)
-        item_embedding = self.item_embedding(item_ids)
+        user_embedding = self.user_embedding_layer(user_ids)
+        item_embedding = self.item_embedding_layer(item_ids)
         predicted_interaction_probability = tf.reduce_sum(user_embedding * item_embedding, axis=1)
         return predicted_interaction_probability
+    
+    def fit(
+        self, 
+        train_dataset: tf.data.Dataset,
+        test_dataset: tf.data.Dataset = None,
+        nepochs: int = 1,
+        shuffle: bool = True,
+        batch_size: int = 16384,
+        **kwargs
+    ):
+        train_dataset_length = len(train_dataset)
+        test_dataset_length = len(test_dataset) if test_dataset is not None else 0
+        if shuffle:
+            train_dataset = train_dataset.shuffle(buffer_size=train_dataset_length)
+        if batch_size is not None and batch_size > 1:
+            train_dataset = train_dataset.batch(batch_size)
+        
+        for epoch in range(nepochs):
+            self.reset_metrics()
 
+            # Training Loop
+            with tqdm(total=train_dataset_length, ncols=100, desc=f"[{epoch+1}/{nepochs}]") as pbar:
+                for step, training_batch in enumerate(train_dataset):
+                    user_ids = training_batch["user_id"]
+                    item_ids = training_batch["item_id"]
+
+                    # random negative sample
+                    random_negatives = self.sampler.sample(user_ids)
+                    
+                    # forward pass
+                    with tf.GradientTape() as tape:
+                        user_embedding = self.user_embedding_layer(user_ids)
+                        item_embedding = self.item_embedding_layer(item_ids)
+                        negative_embedding = self.item_embedding_layer(random_negatives)
+
+                        positive_prediction = tf.reduce_sum(user_embedding * item_embedding, axis=-1)
+                        negative_prediction = tf.reduce_sum(user_embedding * negative_embedding, axis=-1)
+
+                        loss_value = tf.reduce_mean(-tf.math.log(tf.sigmoid(positive_prediction - negative_prediction)))
+
+                    # calculate gradient
+                    user_gradient, item_gradient = tape.gradient(loss_value, self.user_embedding_layer.trainable_variables + self.item_embedding_layer.trainable_variables)
+
+                    # back propagation
+                    self.optimizer.apply_gradients(zip([user_gradient, item_gradient], self.user_embedding_layer.trainable_variables + self.item_embedding_layer.trainable_variables))
+
+                    # update training loss
+                    self.train_loss_tracker.update_state(loss_value)
+
+                    pbar.update(len(user_ids))
+                    pbar.set_postfix({"loss": float(self.train_loss_tracker.result())})
+            self.train_loss_history.append(float(self.train_loss_tracker.result()))
+
+            # # Validation Loop
+            # if test_dataset is not None:
+            #     self.evaluate(test_dataset, batch_size=batch_size)
+
+    def evaluate(
+        self,
+        dataset: tf.data.Dataset,
+        batch_size: int = 16384,
+        **kwargs
+    ):
+        dataset_length = len(dataset)
+        if batch_size is not None and batch_size > 1:
+            dataset = dataset.batch(batch_size)
+        
+        for step, evaluation_batch in enumerate(dataset):
+            user_ids = evaluation_batch["user_id"]
+            item_ids = evaluation_batch["item_id"]
+
+            # random negative sample
+            random_negatives = self.sampler.sample(user_ids)
+
+            # forward pass
+            user_embedding = self.user_embedding_layer(user_ids)
+            item_embedding = self.item_embedding_layer(item_ids)
+            negative_embedding = self.item_embedding_layer(random_negatives)
+
+            positive_prediction = tf.reduce_sum(user_embedding * item_embedding, axis=-1)
+            negative_prediction = tf.reduce_sum(user_embedding * negative_embedding, axis=-1)
+
+            loss_value = tf.reduce_mean(-tf.math.log(tf.sigmoid(positive_prediction - negative_prediction)))
+
+            # update test loss
+            self.test_loss_tracker.update_state(loss_value)
+        self.test_loss_history.append(float(self.test_loss_tracker.result()))
+
+        return float(self.test_loss_tracker.result())
---
+++ src/sampler/__init__.py
@@ -0,0 +1,17 @@
+import numpy as np
+import tensorflow as tf
+
+from typing import Iterable, Dict
+
+class SimpleSampler:
+    def __init__(self, item_set: Iterable[int], user_items: Dict[int, Iterable[int]]):
+        self.item_set = item_set
+        self.user_items = user_items
+
+    def sample(self, user_ids: Iterable[int]) -> tf.Tensor:
+        random_negatives = np.random.choice(self.item_set, size=len(user_ids))
+        for i, uid in enumerate(np.array(user_ids)):
+            while random_negatives[i] in self.user_items[uid]:
+                random_negatives[i] = np.random.choice(self.item_set)
+        random_negatives = tf.constant(random_negatives)
+        return random_negatives
